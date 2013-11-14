a1f05ccbaa8218c74f675501d00f268a0e29a7a4
Nikki Thean <nikki.thean@gmail.com>
2013-11-14 18:30:34 -0500

    Add small changes

--- encoder.rb
+++ encoder.rb
@@ -14,7 +14,7 @@ def encode(message)
   history = Hash.new(message)
 
   gsub_args.each_with_index do |args, i|
-    history[(i+1)] = history[i].send(:gsub, args[0], args[1])
+    history[(i+1)] = history[i].gsub(args[0], args[1])
   end
 
   puts history.values.last.reverse
